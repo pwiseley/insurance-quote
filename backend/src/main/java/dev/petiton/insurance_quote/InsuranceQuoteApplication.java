4c8c9059f619ec357d5c87268343afc1caa71dc3
Petiton Wiseley Paul-Enzer <wppet@ulaval.ca>
2026-05-25 06:31:09 -0400

    chore: add spring boot backend structure with start.spring.io

---
+++ backend/src/main/java/dev/petiton/insurance_quote/InsuranceQuoteApplication.java
@@ -0,0 +1,13 @@
+package dev.petiton.insurance_quote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceQuoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceQuoteApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/petiton/insurance_quote/InsuranceQuoteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.petiton.insurance_quote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceQuoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
