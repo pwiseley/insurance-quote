7ebdc9f10e6e916c3250f7ab20751cb98fd53932
Petiton Wiseley Paul-Enzer <wppet@ulaval.ca>
2026-07-21 09:07:55 -0400

    fix: add a schema annotation to correct 504 error on swagger access

--- backend/src/main/java/dev/petiton/insurancequote/dto/QuoteRequest.java
+++ backend/src/main/java/dev/petiton/insurancequote/dto/QuoteRequest.java
@@ -2,12 +2,16 @@ package dev.petiton.insurancequote.dto;
 
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-
+@Schema(
+        oneOf = { AutoQuoteRequest.class, HomeQuoteRequest.class },
+        discriminatorProperty = "type"
+)
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
 @JsonSubTypes({
         @JsonSubTypes.Type(value = AutoQuoteRequest.class, name = "AUTO"),
         @JsonSubTypes.Type(value = HomeQuoteRequest.class, name = "HOME")
 })
-public sealed interface QuoteRequest permits AutoQuoteRequest, HomeQuoteRequest{
+public sealed interface QuoteRequest permits AutoQuoteRequest, HomeQuoteRequest {
 }
